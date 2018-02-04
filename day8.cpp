983b88ae0c2cdc4f560147128a7ac04f01949836
Nitish Chauhan <nitish.nc18@gmail.com>
2018-02-05 00:08:45 +0530

    https://www.hackerrank.com/contests/hourrank-26/challenges/combo-meal/problem

---
+++ day8.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int profit(int b, int s, int c) {
+    // Return the fixed profit.
+}
+
+int main() {
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        int b;
+        int s;
+        int c;
+        cin >> b >> s >> c;
+        int result = profit(b, s, c);
+        cout << result << endl;
+    }
+    return 0;
+}
+
