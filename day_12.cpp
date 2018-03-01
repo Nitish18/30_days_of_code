9dcad610021d53e9369c60d1cb763dcd87b6502d
Nitish Chauhan <nitish.nc18@gmail.com>
2018-03-01 23:50:38 +0530

    https://www.hackerearth.com/challenge/competitive/march-easy-18/algorithm/holi-and-colorful-houses-eb2049cb/

---
+++ day_12.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    
+    int T;
+    cin >> T;
+    while(T--){
+        int N,Q;
+        cin >> N;
+        cin >> Q;
+        string s;
+        cin >> s;
+        for(int i=0;i<Q;i++){
+            int x1,x2;
+            cin >> x1;
+            cin >> x2;
+            
+            
+            
+        }
+        
+        
+    }
+    
+}
